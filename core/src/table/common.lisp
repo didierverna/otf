59502882bf51b1894779d8fe41b0e96de78df38c
Didier Verna <didier@didierverna.net>
2023-07-27 16:45:48 +0200

    Implement unsupported table skipping and padding check.
    2023-07-27  Didier Verna  <didier@didierverna.net>
    
    	* core/src/table/common.lisp (spurious-table-byte): New condition.
    	(read-table): New before method to skip padding. Read an discard
    	unsupported table contents.

--- core/src/table/common.lisp
+++ core/src/table/common.lisp
@@ -62,12 +62,45 @@
   (:documentation "The Unsupported Table usage warning.
 It signals that an OTF table is unsupported."))
 
+(define-condition spurious-table-byte (otf-compliance-error)
+  ((table-name
+    :initarg :table-name
+    :reader table-name)
+   (spurious-byte-position
+    :initarg :spurious-byte-position
+    :reader spurious-byte-position)
+   (table-position
+    :initarg :table-position
+    :reader table-position))
+  (:report (lambda (spurious-table-byte stream)
+	     (report stream "Spurious non-zero byte in table padding.
+At position ~A, before the start of table '~A' at position ~A."
+		     (spurious-byte-position spurious-table-byte)
+		     (table-name spurious-table-byte)
+		     (table-position spurious-table-byte))))
+  (:documentation "The Spurious Table Byte compliance error.
+It signals that a non-zero byte was encountered in a table padding."))
+
 (defgeneric read-table (font name record)
-  (:documentation "Read a new table into FONT.
+  (:documentation "Read a new table from *STREAM* into FONT.
 NAME is the table's name as a Lisp keyword.
-RECORD is the corresponding table record from the tables directory.")
+RECORD is the corresponding table record from the tables directory.
+The file position in *STREAM* must be before, or exactly at the beginning of
+the table to be read, and strictly after the contents of the previous table,
+if any.")
+  (:method :before (font name record)
+    "Skip potential padding before the beginning of the table."
+    (assert (<= (file-position *stream*) (table-record-offset record)))
+    (dotimes (i (- (table-record-offset record) (file-position *stream*)))
+      (unless (= (read-byte *stream*) #x00)
+	(with-simple-restart (ignore-spurious-byte "Ignore spurious byte.")
+	  (error 'spurious-table-byte
+		 :table-name (table-record-tag record)
+		 :spurious-byte-position (1- (file-position *stream*))
+		 :table-position (table-record-offset record))))))
   (:method (font name record)
     "Skip unsupported table. This is the default method."
-    (warn 'unsupported-table :name (table-record-tag record))))
+    (warn 'unsupported-table :name (table-record-tag record))
+    (dotimes (i (table-record-length record)) (read-byte *stream*))))
 
 ;;; common.lisp ends here
